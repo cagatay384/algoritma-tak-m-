8c7f6cef3b7624371d920059e7062d6f84cf99d7
cagatay384 <384cagatayonbasioglu@gmail.com>
2025-03-03 09:02:55 +0300

    Add files via upload

---
+++ SumOfChildren.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def checkTree(self, root:TreeNode) -> bool:
+        if not root:
+            return False
+        else:
+            if root.val == root.right.val + root.left.val:
+                return True
+            else:
+                return False
+
+
+
+
+
